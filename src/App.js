61c0cbfc6f062dd24a88762317248ba4ae0f24a9
Muneer PP <abdulmuneer22@gmail.com>
2017-02-22 14:13:06 +0530

    added redux support

--- src/App.js
+++ src/App.js
@@ -4,12 +4,26 @@ import './App.css';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
 
+
+import { Provider } from 'react-redux'
+import { createStore  , applyMiddleware} from 'redux'
+
+import reducers from './Redux/Reducers'
+import thunk from 'redux-thunk';
+
+const store = createStore(
+  reducers,
+  applyMiddleware(thunk)
+);
+
 import Nomad from './Nomad'
 
 class App extends Component {
   render() {
     return (
+      <Provider store={store}>
       <Nomad />
+      </Provider>
     );
   }
 }
